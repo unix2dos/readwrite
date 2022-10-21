d592db52861969c6d96dceec88a1115fc1f208a7
liuwei <liuwei@funlink-tech.com>
2022-10-21 15:29:51 +0800

    update public

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dohttp.com
+
++ 随时随记
+
+```bash
+ln -s source/_posts/think/随笔/2022_10.md write
+```
+
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+
+###### 日记 ########
+target_file="./source/index.md"
+rm -rf $target_file
+search_dir="./source/_posts/think/随笔"
+for entry in "$search_dir"/*
+do
+	value="$(cat $entry)"
+	echo "$value" >> $target_file
+done
+
+
+
+
+#######  博客 ######
 hexo clean --config source/_data/next.yml && hexo g --config source/_data/next.yml && hexo g -d --config source/_data/next.yml 
 git add . && git commit -m "update public" 
 git pull && git push
