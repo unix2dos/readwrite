b32c3371214305b5773f5eba530bd2300ead1659
unix2dos <liuwei@funlink-tech.com>
2022-10-26 12:15:05 +0800

    add daily

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,6 @@
 
 
 ###### 日记 ########
-target_file="./source/index.md"
-rm -rf $target_file
-search_dir="./source/_posts/think/随笔"
-for entry in "$search_dir"/*
-do
-	value="$(cat $entry)"
-	echo "$value" >> $target_file
-done
 
 
 
