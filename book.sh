a7693b309e6c546abeb3d910690b3c9fdda8ce48
unix2dos <levonfly@gmail.com>
2023-03-21 12:03:30 +0800

    update book dir

---
+++ book.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+src="/Users/liuwei/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents/Obsidian/微信读书"
+dest="/Users/liuwei/workspace/readwrite.fun/source/_posts/c_booking/01进行中"
+
+mv "$src" "$dest"
