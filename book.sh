92527bc652e3807399863328fa58e059a005b06c
unix2dos <levonfly@gmail.com>
2023-03-21 12:21:18 +0800

    update book.sh

--- book.sh
+++ book.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/zsh
 
-src="/Users/liuwei/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents/Obsidian/微信读书"
-dest="/Users/liuwei/workspace/readwrite.fun/source/_posts/c_booking/01进行中"
 
-mv "$src" "$dest"
+
+
+mv -f /Users/liuwei/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents/Obsidian/微信读书/* /Users/liuwei/workspace/readwrite.fun/source/_posts/c_booking/01进行中/
