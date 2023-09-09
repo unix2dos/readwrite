b9f58c5a3c1f9f72b8808dedd98f0fc81faaa276
unix2dos <levonfly@gmail.com>
2023-09-09 19:12:58 +0800

    update public

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@
 
 
 #######  博客 ######
+git pull
 hexo clean --config source/_data/next.yml && hexo g --config source/_data/next.yml && hexo g -d --config source/_data/next.yml 
 git add . && git commit -m "update public" 
 git pull && git push
