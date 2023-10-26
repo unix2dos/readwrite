ebf5794ce5bebfa060c4e2951b44d8976b378c52
unix2dos <levonfly@gmail.com>
2023-10-26 19:14:17 +0800

    修改books 结构

--- book.sh
+++ book.sh
@@ -3,4 +3,4 @@
 
 
 
-mv -f /Users/liuwei/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents/Obsidian/微信读书/* /Users/liuwei/workspace/readwrite/source/_posts/4_booking/01进行中/
+mv -f /Users/liuwei/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents/Obsidian/微信读书/* /Users/liuwei/workspace/readwrite/source/booking/01进行中/
