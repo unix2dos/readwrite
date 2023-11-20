eb48a0b94bebd6df642637caef8f8cd46fa5c52a
unix2dos <levonfly@gmail.com>
2023-11-20 10:10:14 +0800

    update books

--- book.sh
+++ book.sh
@@ -1,6 +1,4 @@
 #!/bin/zsh
 
 
-
-
-mv -f /Users/liuwei/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents/Obsidian/微信读书/* /Users/liuwei/workspace/readwrite/source/booking/01进行中/
+mv -f /Users/liuwei/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents/Obsidian/44_Map/微信读书/* /Users/liuwei/workspace/readwrite/source/booking/01进行中/
