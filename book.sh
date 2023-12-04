60bbadda4f4a4f5fb04c7ca8bc80f58618fbeb23
unix2dos <levonfly@gmail.com>
2023-12-04 19:26:40 +0800

    增加图书

--- book.sh
+++ book.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 
-mv -f /Users/liuwei/Dropbox/Obsidian/44_Map/微信读书/* /Users/liuwei/workspace/readwrite/source/booking/01进行中/
+mv -f /Users/liuwei/Dropbox/Obsidian/library/44_Map/微信读书/* /Users/liuwei/workspace/readwrite/source/booking/01进行中/
