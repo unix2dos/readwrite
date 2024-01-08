6f4c29dc89c0d1ee8b5538a7a3c45d19a1bbb4ab
unix2dos <levonfly@gmail.com>
2024-01-08 21:53:09 +0800

    update shelll

--- book.sh
+++ book.sh
@@ -1,4 +1,30 @@
 #!/bin/zsh
 
 
-mv -f /Users/liuwei/Dropbox/Obsidian/library/400_Map/微信读书/* /Users/liuwei/workspace/readwrite/source/booking/01进行中/
+#mv -f /Users/liuwei/Dropbox/Obsidian/library/400_Map/微信读书/* /Users/liuwei/workspace/readwrite/source/booking/01进行中/
+
+dir='/Users/liuwei/workspace/readwrite/source/booking/01进行中/'
+cd $dir
+subfolder="old_dir"
+if [ ! -d "$subfolder" ]; then
+    mkdir "$subfolder"
+fi
+
+for file in *; do
+	if [ -f "$file" ]; then
+		if grep -q '^date' "$file"; then
+			today=$(date +%Y-%m-%d)
+			input_date=$(grep '^date' "$file")
+			input_date=$(echo "$input_date" | awk '{print $2}')
+			input_timestamp=$(date -jf "%Y-%m-%d" "$input_date" "+%s")
+			today_timestamp=$(date -jf "%Y-%m-%d" "$today" "+%s")
+			time_diff=$(( (today_timestamp - input_timestamp) / 86400 ))
+			if [ "$time_diff" -gt 30 ]; then
+		 		echo $time_diff "$file"
+				mv "$file" "$subfolder/"
+			fi
+		 	#echo $time_diff "$file"
+		fi
+	fi
+done
+
